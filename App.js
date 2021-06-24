d56710726df8ab7ea435409e9796c5f63318a4f9
adildulobu <adildulobu@gmail.com>
2021-06-24 23:23:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import AddChatScreen from './screens/AddChatScreen';
 import { LogBox } from 'react-native';
+import ChatScreen from './screens/ChatScreen';
 const Stack = createStackNavigator();
 
 const globalScreenOptions = {
@@ -26,6 +27,8 @@ export default function App() {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="AddChat" component={AddChatScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+
         </Stack.Navigator>
     </NavigationContainer>
   );
