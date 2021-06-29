90a472e40caac31c24cb6e410385e77c1af2849a
adildulobu <adildulobu@gmail.com>
2021-06-29 11:33:06 +0200

    Done!!!

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,29 +1,51 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { ListItem, Avatar } from "react-native-elements";
+import { db } from "../firebase";
 
-const CustomListItem = ({id, chatName, enterChat}) =>{
+const CustomListItem = ({ id, chatName, enterChat }) => {
+	const [chatMessages, setChatMessages] = useState([]);
 
-    return(
-        <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider> 
-            <Avatar 
-                rounded
-                source={{
-                    uri:
-                        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png"
-                }}
-            />
-            <ListItem.Content>
-            <ListItem.Title style={{fontWeight: "900"}}>{chatName}</ListItem.Title>
-                <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                    ABC
-                </ListItem.Subtitle>
-            </ListItem.Content>
-        </ListItem>
-    )
+	useEffect(() => {
+		const unsubscribe = db
+			.collection("chats")
+			.doc(id)
+			.collection("messages")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) =>
+				setChatMessages(snapshot.docs.map((doc) => doc.data()))
+			);
+		return unsubscribe;
+	}, []);
 
+	return (
+		<ListItem
+			key={id}
+			onPress={() => enterChat(id, chatName)}
+			key={id}
+			bottomDivider
+		>
+			<Avatar
+				rounded
+				source={{
+					uri:
+						chatMessages?.[0]?.photoURL ||
+						"http://www.cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+				}}
+			/>
 
-}
-export default CustomListItem
+			<ListItem.Content>
+				<ListItem.Title style={{ fontWeight: "800" }}>
+					{chatName}
+				</ListItem.Title>
+				<ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
+					{chatMessages?.[0]?.displayName}:{chatMessages?.[0]?.message}
+				</ListItem.Subtitle>
+			</ListItem.Content>
+		</ListItem>
+	);
+};
 
-const styles = StyleSheet.create({})
+export default CustomListItem;
+
+const styles = StyleSheet.create({});
